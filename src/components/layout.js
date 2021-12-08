c5a6d170d9a4327669a608ac946d42d8a5cda786
Matt Zhan <mzhan1@godaddy.com>
2021-12-08 13:47:47 -0600

    wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,12 +25,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
       <div
         style={{
           margin: `0 auto`,
           // maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          // padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
